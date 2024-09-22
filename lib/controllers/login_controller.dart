7e807826dc9a1581047f632839e4694b208acdea
Santiago Heras <santiha121@gmail.com>
2024-09-22 14:50:56 -0500

    Añadido controlador del login

---
+++ lib/controllers/login_controller.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+final TextEditingController usernameController = TextEditingController();
+final TextEditingController passwordController = TextEditingController();

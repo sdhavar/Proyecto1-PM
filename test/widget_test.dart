1a8e832ac8d8affce05410e441dc2a71a9e103c6
Santiago Heras <santiha121@gmail.com>
2024-09-22 14:52:22 -0500

    Código verificado, ya funcionando

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,28 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:flutter_application_1/main.dart';
+import 'package:flutter_application_1/login_page.dart'; // Cambiado a login_page.dart
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Login screen test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(LoginPage());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the login screen is displayed.
+    expect(find.text('LOGIN'), findsOneWidget);
+    expect(find.byIcon(Icons.person), findsOneWidget);
+    expect(find.byIcon(Icons.lock), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Enter username and password.
+    await tester.enterText(find.byType(TextField).at(0), 'testuser');
+    await tester.enterText(find.byType(TextField).at(1), 'password');
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Tap the login button.
+    await tester.tap(find.byIcon(Icons.check));
+    
+    // Wait for the navigation to complete.
+    await tester.pumpAndSettle();
+
+    // Verify navigation to MainPage.
+    expect(find.text('Main Page'), findsOneWidget);
   });
 }

c6632724f43b1e76e3768bb5a460963f156b178f
Santiago Heras <santiha121@gmail.com>
2024-09-30 23:50:52 -0500

    Actualizado el diseño de la main page

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -59,63 +59,137 @@ class MainPage extends StatelessWidget {
             ),
           ),
           // Contenido principal
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Center(
+          SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
               child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  // Título con el nombre del usuario
-                  Text(
-                    'Hola, $username',
-                    style: TextStyle(
-                      fontSize: 36,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                      fontFamily: 'Roboto', // Cambié la tipografía
+                  // Encabezado
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Hola, $username',
+                              style: TextStyle(
+                                fontSize: 36,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                                fontFamily: 'Roboto',
+                              ),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              'Aquí puedes llevar un control diario de tus consumos y ganar recompensas por tus hábitos saludables.',
+                              textAlign: TextAlign.left,
+                              style: TextStyle(fontSize: 18, color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          color: Color(0xFF034f84),
+                          shape: BoxShape.circle,
+                        ),
+                        child: Icon(
+                          Icons.person,
+                          color: Colors.white,
+                          size: 32,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 80), // Aumentar el espacio para bajar los contenedores
+                  // Contenedores centrados
+                  Center(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            CustomContainer(
+                              text: 'Gamificación',
+                              icon: Icons.star,
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => GamificationPage()),
+                                );
+                              },
+                            ),
+                            CustomContainer(
+                              text: 'Encuesta Diaria',
+                              icon: Icons.poll,
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SurveyPage()),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        CustomContainer(
+                          text: 'Registro Diario',
+                          icon: Icons.book,
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => DailyIntakePage()),
+                            );
+                          },
+                        ),
+                      ],
                     ),
                   ),
                   SizedBox(height: 20),
-                  // Párrafo explicativo
-                  Text(
-                    'Aquí puedes llevar un control diario de tus consumos y ganar recompensas por tus hábitos saludables.',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 18, color: Colors.white),
+                ],
+              ),
+            ),
+          ),
+          // Barra inferior
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              color: Colors.grey[200],
+              padding: EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Column(
+                    children: [
+                      Icon(Icons.home, color: Colors.grey[700]),
+                      Text(
+                        'Home',
+                        style: TextStyle(
+                          color: Colors.blue[700],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
-                  SizedBox(height: 40),
-                  // Botón para ir a la página de gamificación
-                  CustomButton(
-                    text: 'Gamification',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => GamificationPage()),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  // Botón para ir a la encuesta diaria de consumo
-                  CustomButton(
-                    text: 'Encuesta Diaria',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => SurveyPage()),
-                      );
-                    },
-                  ),
-                  SizedBox(height: 20),
-                  // Botón para el registro de datos de consumo diario
-                  CustomButton(
-                    text: 'Registro Diario',
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => DailyIntakePage()),
-                      );
-                    },
+                  Column(
+                    children: [
+                      Icon(Icons.settings, color: Colors.grey[700]),
+                      Text(
+                        'Settings',
+                        style: TextStyle(
+                          color: Colors.blue[700],
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -127,31 +201,47 @@ class MainPage extends StatelessWidget {
   }
 }
 
-class CustomButton extends StatelessWidget {
+// Widget personalizado para los contenedores
+class CustomContainer extends StatelessWidget {
   final String text;
-  final VoidCallback onPressed;
+  final IconData icon;
+  final VoidCallback onTap;
 
-  CustomButton({required this.text, required this.onPressed});
+  CustomContainer({required this.text, required this.icon, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: Color(0xFF034f84), // Color de fondo renovado
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(30), // Bordes más redondeados
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 150, // Ancho fijo para todos los contenedores
+        height: 150, // Alto fijo para todos los contenedores
+        padding: EdgeInsets.symmetric(horizontal: 28, vertical: 14),
+        decoration: BoxDecoration(
+          color: Color(0xFF034f84), // Color del contenedor
+          borderRadius: BorderRadius.circular(20), // Bordes redondeados
         ),
-        padding: EdgeInsets.symmetric(horizontal: 28, vertical: 14), // Aumenté el padding
-      ),
-      child: Text(
-        text,
-        style: TextStyle(color: Colors.white, fontSize: 18), // Cambié tamaño de fuente
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(icon, color: Colors.white, size: 48),
+            SizedBox(height: 10),
+            Text(
+              text,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18, // Cambié el tamaño de la fuente
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
+// Círculos decorativos (ya estaban en el código)
 class Circle extends StatelessWidget {
   final double diameter;
   final Color color;
